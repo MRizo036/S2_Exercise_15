6ea9a5647869d64c46e0342ad7ce64cf99946c01
Unknown <mrizo036@west-mec.org>
2019-04-18 16:45:26 -0700

    Final Commit
    All HTML and JavaScript code completed. Ready for Review.

--- co_payment.js
+++ co_payment.js
@@ -85,25 +85,23 @@ function validateCVC() {
       var creditCard = document.querySelector('input[name="credit"]:checked').value;
 
       if (cardCVC.validity.valueMissing) {
-            cardCVC.setCustomValidity("Enter your CVC number")
+            cardCVC.setCustomValidity("Enter your CVC number");
       } else if ((creditCard === "amex") && (/^\d{4}$/.test(cardCVC.value) === false)) {
-            cardCVC.setCustomValidity("")
-      } else if ( && ) {
-
+            cardCVC.setCustomValidity("Enter a 4-digit CVC number");
+      } else if ((creditCard !== "amex") && (/^\d{3}$/.test(cardCVC.value) === false)) {
+            cardCVC.setCustomValidity("Enter a 3-digit CVC number");
       } else {
-
+            cardCVC.setCustomValidity("");
       }
 }
-
-
 
 function validateMonth() {
       var cardMonth = document.getElementById("expMonth");
 
       if (cardMonth.selectedIndex === 0) {
-            cardMonth.setCustomValidity("Select the expiration month")
+            cardMonth.setCustomValidity("Select the expiration month");
       } else {
-            cardMonth.setCustomValidity("")
+            cardMonth.setCustomValidity("");
       }
 }
 
@@ -111,9 +109,9 @@ function validateYear() {
       var cardYear = document.getElementById("expYear");
 
       if (cardYear.selectedIndex === 0) {
-            cardYear.setCustomValidity("Select the expiration year")
+            cardYear.setCustomValidity("Select the expiration year");
       } else {
-            cardYear.setCustomValidity("")
+            cardYear.setCustomValidity("");
       }
 }
 
@@ -124,8 +122,10 @@ function validateNumber() {
             cardNumber.setCustomValidity("Enter your card number");
       } else if (cardNumber.validity.patternMismatch) {
             cardNumber.setCustomValidity("Enter a valid card number");
+      } else if (luhn(cardNumber.value) === false) {
+            cardNumber.setCustomValidity("Enter a legitimate card number")
       } else {
-            cardNumber.setCustomValidity("")
+            cardNumber.setCustomValidity("");
       }
 }
 
@@ -145,4 +145,30 @@ function validateName() {
       } else {
             cardName.setCustomValidity("");
       }
+}
+
+function sumDigits(numStr) {
+      var digitTotal = 0;
+      for (var i = 0; i < numStr.length; i++) {
+            digitTotal += parseInt(numStr.charAt(i));
+      }
+      return digitTotal;
+}
+
+function luhn(idNum) {
+      var string1 = "";
+      var string2 = "";
+
+      //Retrieve the odd-number digits
+      for (var i = idNum.length - 1; i >= 0; i -= 2) {
+            string1 += idNum.charAt(i);
+      }
+
+      //Retrieve the even-number digits and double them
+      for (var i = idNum.length - 2; i >= 0; i -= 2) {
+            string2 += 2 * idNum.charAt(i);
+      }
+
+      //Return whether the sum of the digits is divisible by 10
+      return sumDigits(string1 + string2) % 10 === 0;
 }
